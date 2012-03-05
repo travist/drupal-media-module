8670623498bc05ee09fea9d36c4267354e1ad158
Dave Reid <dave@davereid.net>
2012-03-05 09:35:08 -0600

    Issue #1400200: Switched from using the CTools plugin system to our own.

--- media.api.php
+++ media.api.php
@@ -12,22 +12,38 @@
  * return a nested array of plugin information, keyed by plugin name. Each
  * plugin info array may have the following keys:
  * - title (required): A name for the tab in the media browser.
- * - handler (required): The class name of the handler. This class must
+ * - class (required): The class name of the handler. This class must
  *   implement a view() method, and may (should) extend the
  *   @link MediaBrowserPlugin MediaBrowserPlugin @endlink class.
  * - weight (optional): Integer to determine the tab order. Defaults to 0.
  * - access callback (optional): A callback for user access checks.
  * - access arguments (optional): An array of arguments for the user access
  *   check.
+ *
  * Additional custom keys may be provided for use by the handler.
+ *
+ * @see hook_media_browser_plugin_info_alter()
+ * @see media_get_browser_plugin_info()
  */
 function hook_media_browser_plugin_info() {
-  $plugins['media_upload'] = array(
+  $info['media_upload'] = array(
     'title' => t('Upload'),
-    'handler' => 'MediaBrowserUpload',
+    'class' => 'MediaBrowserUpload',
     'weight' => -10,
     'access callback' => 'user_access',
     'access arguments' => array('create files'),
   );
-  return $plugins;
+
+  return $info;
+}
+
+/**
+ * Alter the list of plugins for the media browser.
+ *
+ * @see hook_media_browser_plugin_info()
+ * @see media_get_browser_plugin_info()
+ */
+function hook_media_browser_plugin_info_alter() {
+  $info['media_upload']['title'] = t('Upload 2.0');
+  $info['media_upload']['class'] = 'MediaBrowserUploadImproved';
 }
